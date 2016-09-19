afe8db0a9ebf27848edd1c85763d9c557348796f
Caysche <cay.cornelius@gmail.com>
2016-09-18 19:33:07 -0700

    Initial commit

---
+++ FastestRPM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastestRPM
+//
+//  Created by Cay Cornelius on 18/09/16.
+//  Copyright © 2016 Cornelius.Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FastestRPM/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FastestRPM
+//
+//  Created by Cay Cornelius on 18/09/16.
+//  Copyright © 2016 Cornelius.Media. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
